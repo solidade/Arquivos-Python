702439c96fa7e056dd20827c3a22a3516a462a53
Miqueias Solidade Pereira <solidade.s.p@gmail.com>
2021-11-04 14:16:05 -0300

    Atualizei para o padrão fstring

--- PYTON_ARQUIVOS/mundo_01/aniversario.py
+++ PYTON_ARQUIVOS/mundo_01/aniversario.py
@@ -2,4 +2,4 @@ ano = input("Em que ano você nasceu?")
 mes = input("Qual o mês?")
 dia = input("Em qual dia?")
 
-print("Sua data de nascimento é:", dia+"/"+mes+"/"+ano)
+print(f"Sua data de nascimento é: {dia}/{mes}/{ano}")
